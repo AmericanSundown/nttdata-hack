29d6778b4cc4d9e8931a385f82f92307d732a34e
niba1122 <niba1122@gmail.com>
2015-02-21 17:10:20 +0900

    drawLines added using test data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 angular.module("myapp",[])
 	.controller("MapController",["$scope","MapService",function ($scope,MapService) {
 	    var point = new google.maps.LatLng(35.658892, 139.755286);
-	    var zoom = 15;
+	    var zoom = 3;
 
 	    MapService.initMap(point,zoom);
 
@@ -16,9 +16,10 @@ angular.module("myapp",[])
 		var map;
 		var nodes = [];
 		var ways = [];
+		var lines = [];
 
 		function initMap(point,zoom) {
-			var map = new google.maps.Map(document.getElementById("map"), {
+			map = new google.maps.Map(document.getElementById("map"), {
 				mapTypeId: google.maps.MapTypeId.ROADMAP,
 				center: point,
 				zoom: zoom
@@ -30,7 +31,7 @@ angular.module("myapp",[])
 
 		function showWayWidth(url) {
 			getData(url, function () {
-				alert("ok!")
+				drawLines();
 			})
 
 		}
@@ -40,24 +41,64 @@ angular.module("myapp",[])
 				method: 'GET',
 				url: url.nodes,
 				timeout: 100000
-			}).success(function (data) {
-				nodes = data;
-			});
-
+			})
 
 			var ways_promise = $http({
 				method: 'GET',
 				url: url.ways,
 				timeout: 100000
-			}).success(function (data) {
-				ways = data
-			});
+			})
 
 			$q.all([nodes_promise,ways_promise]).then(function (data) {
 				// nodes.push(data[0]);
 				// ways.push(data[1]);
+
+				// var flightPlanCoordinates = [
+				ways.push(
+					{
+						id: 1,
+						nodes: [0,1,2,3],
+						minW: 1.5,
+						maxW: 3
+					},
+					{
+						id: 1,
+						nodes: [4,5,6,7],
+						minW: 1.5,
+						maxW: 3
+					}
+				);
+				nodes = [
+					{ id: 0, lat: 37.772323, lon: -122.214897},
+					{ id: 1, lat: 21.291982, lon: -157.821856},
+					{ id: 2, lat: -18.142599, lon: 178.431},
+					{ id: 3, lat: -27.46758, lon: 153.027892},
+					{ id: 4, lat: -37.772323, lon: -122.214897},
+					{ id: 5, lat: -21.291982, lon: -157.821856},
+					{ id: 6, lat: 18.142599, lon: 178.431},
+					{ id: 7, lat: 27.46758, lon: 153.027892}
+				];
 				callback();
 			});
+		}
+		function drawLines() {
+			for (var i=0; i<ways.length; i++) {
+				line_coords = [];
+				for (var j=0; j<ways[i].nodes.length; j++) {
+					var k = ways[i].nodes[j];
+					line_coords.push(new google.maps.LatLng(nodes[k].lat, nodes[k].lon))
+
+					lines[i] = new google.maps.Polyline({
+						path: line_coords,
+						strokeColor: "#FF0000",
+						strokeOpacity: 1.0,
+						strokeWeight: 2
+					});
+ 	 				lines[i].setMap(map);
+				}
+			}
+			alert(map);
+
 		}
 
 		return {
