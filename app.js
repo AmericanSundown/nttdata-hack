08911ee2107c2542f6c64e4f34d24d937693bc9a
niba1122 <niba1122@gmail.com>
2015-02-21 17:59:37 +0900

    revised section of reading jsonp

--- app.js
+++ app.js
@@ -37,47 +37,50 @@ angular.module("myapp",[])
 		}
 
 		function getData(url,callback) {
-			var nodes_promise = $http({
-				method: 'GET',
-				url: url.nodes,
-				timeout: 100000
-			})
+			// var nodes_promise = $http({
+			// 	method: 'GET',
+			// 	url: url.nodes,
+			// 	timeout: 100000
+			// })
 
-			var ways_promise = $http({
-				method: 'GET',
-				url: url.ways,
-				timeout: 100000
-			})
+			// var ways_promise = $http({
+			// 	method: 'GET',
+			// 	url: url.ways,
+			// 	timeout: 100000
+			// })
+
+			var nodes_promise = $http.jsonp(url.nodes);
+			var ways_promise = $http.jsonp(url.ways);
 
 			$q.all([nodes_promise,ways_promise]).then(function (data) {
-				// nodes.push(data[0]);
-				// ways.push(data[1]);
+				alert("callback called!")
+				nodes.push(data[0]);
+				ways.push(data[1]);
 
-				// var flightPlanCoordinates = [
-				ways.push(
-					{
-						id: 1,
-						nodes: [0,1,2,3],
-						minW: 1.5,
-						maxW: 3
-					},
-					{
-						id: 1,
-						nodes: [4,5,6,7],
-						minW: 1.5,
-						maxW: 3
-					}
-				);
-				nodes = [
-					{ id: 0, lat: 37.772323, lon: -122.214897},
-					{ id: 1, lat: 21.291982, lon: -157.821856},
-					{ id: 2, lat: -18.142599, lon: 178.431},
-					{ id: 3, lat: -27.46758, lon: 153.027892},
-					{ id: 4, lat: -37.772323, lon: -122.214897},
-					{ id: 5, lat: -21.291982, lon: -157.821856},
-					{ id: 6, lat: 18.142599, lon: 178.431},
-					{ id: 7, lat: 27.46758, lon: 153.027892}
-				];
+				// ways.push(
+				// 	{
+				// 		id: 1,
+				// 		nodes: [0,1,2,3],
+				// 		minW: 1.5,
+				// 		maxW: 3
+				// 	},
+				// 	{
+				// 		id: 1,
+				// 		nodes: [4,5,6,7],
+				// 		minW: 1.5,
+				// 		maxW: 3
+				// 	}
+				// );
+				// nodes = [
+				// 	{ id: 0, lat: 37.772323, lon: -122.214897},
+				// 	{ id: 1, lat: 21.291982, lon: -157.821856},
+				// 	{ id: 2, lat: -18.142599, lon: 178.431},
+				// 	{ id: 3, lat: -27.46758, lon: 153.027892},
+				// 	{ id: 4, lat: -37.772323, lon: -122.214897},
+				// 	{ id: 5, lat: -21.291982, lon: -157.821856},
+				// 	{ id: 6, lat: 18.142599, lon: 178.431},
+				// 	{ id: 7, lat: 27.46758, lon: 153.027892}
+				// ];
 				callback();
 			});
 		}
